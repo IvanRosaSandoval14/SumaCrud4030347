0f98140951c690f96345336772dde0b80a7ae571
Ivan Rosa <Ivan Rosa@DESKTOP-2VJVQ61>
2024-08-13 12:41:20 -0600

    Agregar archivos de proyecto.

---
+++ SumaCrud4030347/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SumaCrud4030347
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SumaCrud4030347/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SumaCrud4030347
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
